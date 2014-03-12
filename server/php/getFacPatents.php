fd08b0328395a15110544849e83d56e254488be8
unknown <geanne01@github.com>
2014-03-12 23:02:28 +0800

    added getFacPatents.php

---
+++ server/php/getFacPatents.php
@@ -0,0 +1,15 @@
+<?php
+  include("connect.php");
+    if($_GET["fid"]!= NULL){ 
+ 	  $fid = $_GET["fid"];
+  
+      $select_patents = mysql_query("SELECT f.flname, p.paper_title, p.date_publication, p.issuing_country, p.patent_no 
+                                    FROM faculty f, publication p
+								    WHERE p.fid = $fid AND f.fid = p.fid AND p.paper_type='Patent'");
+      $list_patents = array();
+      while($fetch_patents = mysql_fetch_array($select_patents)){
+          $list_patents[] = $fetch_patents;
+      }
+      echo json_encode($list_patents);
+    }	
+?>
