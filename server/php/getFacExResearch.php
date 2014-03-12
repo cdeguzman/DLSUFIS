e61d55dd269da8426e6148b3bfa595dc6ebf2fd3
unknown <geanne01@github.com>
2014-03-12 22:54:17 +0800

    added getFacExResearch.php

---
+++ server/php/getFacExResearch.php
@@ -0,0 +1,15 @@
+<?php
+  include("connect.php");
+    if($_GET["fid"]!= NULL){ 
+ 	  $fid = $_GET["fid"];
+  
+      $select_exproj = mysql_query("SELECT research_title, funding_agency, currency, amount, start_date, end_date 
+                                    FROM research
+								    WHERE fid = $fid AND funding_type='External'");
+      $list_exproj = array();
+      while($fetch_exproj = mysql_fetch_array($select_exproj)){
+          $list_exproj[] = $fetch_exproj;
+      }
+      echo json_encode($list_exproj);
+    }	
+?>
