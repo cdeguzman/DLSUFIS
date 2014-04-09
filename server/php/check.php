d51aa54ceb7d6d893cca51e208ca93a1cb89451e
unknown <geanne01@github.com>
2014-04-09 20:36:24 +0800

    Error checked

--- server/php/check.php
+++ server/php/check.php
@@ -1,19 +1,12 @@
 <?php
  include("connect.php");
- if(isset($_POST["loginBtn"])){
-  $fid = $_POST["userName"];
-  $pass = $_POST["pass"];
+  $fid = $_POST["fid"];
+  $pass = $_POST["password"];
  
-  $select_account = mysql_query("SELECT fid, password FROM faculty WHERE fid = '$fid' AND password = '$pass'");
-  $count_row = mysql_num_rows($select_account); 
- 
-  if($count_row == '1'){
-   while($disp = mysql_fetch_assoc($select_account)){
-     echo $disp["fid"]." ";
-	 //echo $disp["password"];
-   }
-  }
-  
- }
-  
+  $select_account = mysql_query("SELECT fid, password 
+                                 FROM faculty 
+								 WHERE fid = '$fid' 
+								 AND password = '$pass'");
+  $disp = mysql_fetch_assoc($select_account);
+  echo $disp["fid"];
 ?>
