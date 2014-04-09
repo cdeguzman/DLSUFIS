b42b959e270eed63a78b4ddacbeb8dbe00eb8537
unknown <geanne01@github.com>
2014-04-09 19:45:16 +0800

    Added the primary key value (de_id) of the degree_earned table

--- server/php/FacultyAcademicBackground/getFacEarnedDegree.php
+++ server/php/FacultyAcademicBackground/getFacEarnedDegree.php
@@ -3,7 +3,7 @@
     if($_GET["fid"]!= NULL){ 
  	  $fid = $_GET["fid"];
   
-      $select_degree = mysql_query("SELECT de.de_id, dl.dlevel_title, d.degree_title, de.specialization, de.year_obtained, i.institution_name, i.location, de.so_num  
+      $select_degree = mysql_query("SELECT de.de_id as id, dl.dlevel_title, d.degree_title, de.specialization, de.year_obtained, i.institution_name, i.location, de.so_num  
                                     FROM degree_level dl, degree d, degree_earned de, institution i, faculty f 
 									WHERE de.fid = $fid 
 									AND dl.dlevel_id = d.dlevel_id
