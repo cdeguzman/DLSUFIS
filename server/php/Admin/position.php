3043d03951bce40e3dc653f5811a5434d6cc2037
geanne01 <grl.franco01@gmail.com>
2014-10-12 22:30:18 +0800

    Resolve the conflict for position.php

--- server/php/Admin/position.php
+++ server/php/Admin/position.php
@@ -1,11 +1,7 @@
 <?php
   include("../connect.php");
   
-<<<<<<< HEAD
   $select_position = mysql_query("SELECT position_id as id, position_title FROM position ORDER BY position_title");
-=======
-  $select_position = mysql_query("SELECT position_id as id, position_title FROM position ORDER BY position_id");
->>>>>>> 096f9638e8c7ded8143b7803077027d6582c9cde
   $list_position = array();
   
   while($fetch_position = mysql_fetch_assoc($select_position)){
