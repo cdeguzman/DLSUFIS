558b69f352682c7ae26c88a1d659fadddc93431a
geanne01 <grl.franco01@gmail.com>
2014-10-12 22:33:37 +0800

    Resolve conflict for AddNewInstitution.php

--- server/php/Admin/AddNewRecord/addNewInstitution.php
+++ server/php/Admin/AddNewRecord/addNewInstitution.php
@@ -1,24 +1,14 @@
 <?php
   include("../../connect.php");
-<<<<<<< HEAD
-=======
-  $institution_id = $_POST["institution_id"];
->>>>>>> 096f9638e8c7ded8143b7803077027d6582c9cde
   $institution_name = ucwords($_POST["institution_name"]);
   $location = ucwords($_POST["location"]);
   $country = $_POST["country"];
   
   $insertNewInstitution = "INSERT INTO institution
-<<<<<<< HEAD
                   (institution_name, location, country)
                   VALUES 
-				  ('$institution_name', '$location', '$country')";
-=======
-                  (institution_id, institution_name, location, country)
-                  VALUES 
-				  ('$institution_id', '$institution_name', '$location', '$country')";
->>>>>>> 096f9638e8c7ded8143b7803077027d6582c9cde
-  
+		  ('$institution_name', '$location', '$country')";
+
   mysql_query($insertNewInstitution);
   
 ?> 
