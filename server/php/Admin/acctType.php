2834c736eff1be796a89e82d84c6716080b0705d
geanne01 <grl.franco01@gmail.com>
2014-10-12 22:24:38 +0800

    Resolve the conflict for the acctType.php

--- server/php/Admin/acctType.php
+++ server/php/Admin/acctType.php
@@ -1,13 +1,8 @@
 <?php
   include("../connect.php");
   
-<<<<<<< HEAD
   $select_acct = mysql_query("SELECT account_id as id, account_role 
                               FROM account ORDER BY account_role");
-=======
-  $select_acct = mysql_query("SELECT account_id, account_role 
-                              FROM account ORDER BY account_id");
->>>>>>> 096f9638e8c7ded8143b7803077027d6582c9cde
   $list_acct = array();
   
   while($fetch_acct = mysql_fetch_assoc($select_acct)){
