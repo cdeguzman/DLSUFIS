ba8525971b2c057cd8e54ff56817226aac669780
geanne01 <grl.franco01@gmail.com>
2014-10-12 22:28:55 +0800

    Resolve the conflict for location.php

--- server/php/Admin/location.php
+++ server/php/Admin/location.php
@@ -1,11 +1,7 @@
 <?php
   include("../connect.php");
   
-<<<<<<< HEAD
   $select_location = mysql_query("SELECT location_id as id, location_name, region FROM location ORDER BY location_name");
-=======
-  $select_location = mysql_query("SELECT location_id as id, location_name FROM location ORDER BY location_name");
->>>>>>> 096f9638e8c7ded8143b7803077027d6582c9cde
   $list_location = array();
   
   while($fetch_location = mysql_fetch_assoc($select_location)){
